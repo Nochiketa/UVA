3c85808d3e62efd019c476766be314d5d0060aab
Nochiketa <chknochiketa.mir@gmail.com>
2016-11-04 19:57:16 +0600

    Update and rename .gitignore to uva 11172.cpp

---
+++ uva 11172.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t,a,b,sum=0;
+    cin>>t;
+    for(int i=1;i<=t;i++)
+    {
+        cin>>a>>b;
+        if(a<b) cout<<"<"<<endl;
+        else if(a>b) cout<<">"<<endl;
+        else cout<<"="<<endl;
+    }
+    return 0;
+}
+
